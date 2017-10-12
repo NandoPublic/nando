b84490c86a13d61c3df3e029d7eb63f573634869
Hernando Ladino <hernando.ladino@gmail.com>
2017-10-12 11:07:30 -0500

    commit 2

---
+++ Ejercicios/src/Ejecutar/CasaApp.java
@@ -0,0 +1,12 @@
+package Ejecutar;
+
+public class CasaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Casa casa1=new Casa();
+		System.out.println(casa1.getDireccion()+ " "+ casa1.getSuperficie()+ " "+ casa1.getCocina()
+		 + " " + casa1.getHabitacionescasa());
+	}
+
+}
