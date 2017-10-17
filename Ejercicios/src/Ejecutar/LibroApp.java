6dc130a49250a9dcafd5113071775efc0ad39d69
Hernando Ladino <hernando.ladino@gmail.com>
2017-10-17 17:50:46 -0500

    se crea libro 7

--- Ejercicios/src/Ejecutar/LibroApp.java
+++ Ejercicios/src/Ejecutar/LibroApp.java
@@ -11,6 +11,7 @@ public class LibroApp {
 		Libro libro5= new Libro ("asdf 321", "Sublevacion", "", 100);
 
 		Libro libro6= new Libro ("asdf 421", "Arcángel", "N-L", 100);
+		Libro libro7= new Libro ("XXXX 421", "Error Y Sufrimiento", "N-L", 1000);
 
 		System.out.println(libro1.toString());
 		System.out.println(libro2.toString());
